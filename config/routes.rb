e17abe291b7d490df1830eda9e8623163b7ea4d1
cwy007 <chanweiyan007@gmail.com>
2017-11-14 23:37:58 +0800

    user can see their orders

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
     end
   end
   resources :cart_items
-  resources :orders 
+  resources :orders
+  namespace :account do
+    resources :orders 
+  end
   root 'welcome#index'
 end
